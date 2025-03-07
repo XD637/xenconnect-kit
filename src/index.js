01b0a48f4662b90aa15ef3642c92ab8b7a216311
Kishore K <kishore.20ee@kct.ac.in>
2025-03-07 15:40:20 +0530

    added tailwind injection

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import WalletButton from "./components/WalletButton.js";
 import WalletModal from "./components/WalletModal.js";
 import AccountModal from "./components/AccountModal.js";
 
+// Dynamically inject Tailwind styles if in a browser environment
+import tailwindStyles from "./tailwind.js";
+
+if (typeof document !== "undefined") {
+  const styleTag = document.createElement("style");
+  styleTag.innerHTML = tailwindStyles;
+  document.head.appendChild(styleTag);
+}
+
 export {
   XenConnectProvider,
   useXenConnect,
